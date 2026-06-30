32c93ebcbf23c5abeb7f914bffa4b65daf72db57
Will Rice <wrice20@gmail.com>
2026-06-30 15:13:44 -0400

    Solve global-avg-pooling

---
+++ global-avg-pooling/global-avg-pooling.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def global_avg_pool(x):
+    """
+    Compute global average pooling over spatial dims.
+    Supports (C,H,W) => (C,) and (N,C,H,W) => (N,C).
+    """
+    # Write code here
+    if x.ndim < 3:
+        raise ValueError("ndim less than 3")
+    return x.mean(axis=(-2, -1))
